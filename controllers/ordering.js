f007c7101f238b3560151f351295384b1fc0d3ff
johnbean36 <jmbass36@gmail.com>
2024-03-31 15:43:11 -0500

    Added ejs code for a form to add a user

--- controllers/ordering.js
+++ controllers/ordering.js
@@ -10,22 +10,28 @@ function createOrder(req,res){
     res.render('ordering/create', {
         title: "Pizza Orders",
         phoneNumber: value,
-        customer: customer
+        customer: customer,
+        check: false
 });
 }
 
 async function lookup(req, res){
     let customer;
     let value = req.body.phone;
+    let check = false;
     try{
         customer = await Customer.find(`${req.body.phone}`);
+        if(customer){
+            check = true
+        }
     }catch(err){
         console.log(err);
     }
     res.render('ordering/create', {
         title: "Pizza Orders",
         phoneNumber: value,
-        customer: customer
+        customer: customer,
+        check: check
     })
 }
 
--- routes/ordering.js
+++ routes/ordering.js
@@ -5,5 +5,6 @@ const orderingCtrl = require('../controllers/ordering')
 
 router.get('/', loggedIn, orderingCtrl.createOrder);
 router.post('/lookup', loggedIn, orderingCtrl.lookup);
+router.post('/addcust', loggedIn, orderingCtrl.addCust)
 
 module.exports = router;
